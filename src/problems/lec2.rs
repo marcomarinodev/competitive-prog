009a7f8ccbcfe8259e0a8c14afa10b0fc0003bb3
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2024-04-22 18:43:11 +0200

    sliding window max

--- src/problems/lec2.rs
+++ src/problems/lec2.rs
@@ -1,4 +1,4 @@
-
+use std::collections::VecDeque;
 
 pub fn trapping_rain_water(height: Vec<i32>) -> i32 {
     let mut res = 0;
@@ -28,6 +28,46 @@ pub fn trapping_rain_water(height: Vec<i32>) -> i32 {
             }
         }
 
+    }
+
+    res
+}
+
+pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut res =  Vec::new();
+    let mut deque: VecDeque<(i32, usize)> = VecDeque::new();
+    let mut start_window_index = 0;
+
+    for (i, num) in nums.iter().enumerate() {
+
+        // remove element not in window
+        while let Some(&(_element, position)) = deque.front() {
+            if position >= start_window_index {
+                break;
+            }
+            deque.pop_front();
+        }
+
+        // insert the new element in the window, bubbling it up from the tail
+        // and removing all the ones that has value smaller than the element we are 
+        // inserting
+        while let Some(&(last_element, _)) = deque.back() {
+            if last_element >= *num {
+                break;
+            }
+            deque.pop_back();
+        }
+        deque.push_back((*num, i));
+
+        if i >= (k - 1) as usize {
+            // insert max in res
+            if let Some(max_pair) = deque.front() {
+                res.push(max_pair.0);
+            }
+
+            // move forward start_window_index
+            start_window_index += 1;
+        }
     }
 
     res
